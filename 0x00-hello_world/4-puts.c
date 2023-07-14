fd141fef8408b955d63755f3aa3c6a2cb7a5d26c
fobuah <fosufobuah@gmail.com>
2023-07-14 05:28:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 * main - Entry point
 *
-* Return; Always 0 (success)
+* Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
-put ( "\" Programming is like building a multilingual puzzle");
-return (0)
+put ( "\"Programming is like building a multilingual puzzle");
+return (0);
 }
