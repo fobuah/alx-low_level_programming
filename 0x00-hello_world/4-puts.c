e6193583a18067e8fed5c04315ace2a2808f224c
fobuah <fosufobuah@gmail.com>
2023-07-15 14:15:32 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
+* main - this is the main function
 *
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 int main(void)
 {
-put ( "\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle);
 return (0);
 }
