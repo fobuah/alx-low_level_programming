37d2ecbdbd9f2007bceee1b51a6eaeb95dec2946
fobuah <fosufobuah@gmail.com>
2023-07-17 14:10:36 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char C;
+C = 'a';
+while
+(C <= 'z')
+if ((C != 'q' && C != 'e') && C <= 'z')
+putchar(C);
+C++;
+putchar('\n');
+return (0);
+}
