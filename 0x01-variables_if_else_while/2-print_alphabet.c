75b317df0158bab305333c7b1bbf83c8bad67d15
fobuah <fosufobuah@gmail.com>
2023-07-17 12:56:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always (0) (Success)
+*/
+int main(void)
+{
+char C;
+C = 'a';
+while
+(C <= 'z')
+{
+putchar(C);
+C++;
+}
+putchar('\n');
+return (0);
+}
