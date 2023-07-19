7b75bc3cf05fafd03c68e470257c068f831b38e4
fobuah <fosufobuah@gmail.com>
2023-07-19 15:44:23 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <unistd.h>
 /**
 * print_alphabet - Entry point
-* Return 0
 */
 void print_alphabet(void)
 {
-char c = 'a'
-while (c <= 'z')
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-c++;
 }
 _putchar('\n');
 }
